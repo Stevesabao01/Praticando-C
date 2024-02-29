70b9eb5bb6c8929961ebbba813a1464f2d41b1f8
Nico_Oliveira39 <98227758+Stevesabao01@users.noreply.github.com>
2024-02-29 11:39:16 -0800

    Add files via upload

---
+++ Praticando C.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main () {
+        float numero1 , numero2;
+        float soma, subtracao, multiplicacao, divisao;
+
+        printf("*** Calculadora Modelo 1 ***\n");
+        printf("Digite o primeiro valor: ");
+        scanf("%f", &numero1);
+
+        printf("Digite o segundo valor:");
+        scanf("%f",&numero2);
+
+        *\somar*/soma = numero1 + numero2 ;
+        /*subtrair*/subtracao=numero1 - numero2;
+        /*multiplicar*/multiplicacao=numero1 * numero2;
+        /*\dividir*///divisao=numero1 / numero2;
+        if(numero2 !=0)
+            divisao=numero1 / numero2;
+        else{
+             printf("\nDivisao por zero nao permitida.\n");
+             return(-1);
+         }
+
+        printf ("\nA soma de %.2f e %.2f e igual a :% .2f \n" ,)
+}
+
+int main () {
+    
+}
